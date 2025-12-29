fe2509e5d6fd1e97aae2f2f8ae6da56bd29e0a27
s2510423 <s2510423@outlook.kr>
2025-12-29 03:43:30 +0000

    feat: add dirscanner.py (Fix #1)

---
+++ dirscanner.py
@@ -0,0 +1,40 @@
+import header
+import os
+
+header.header(True)
+
+def write_targetlist(name,targetlist):
+    if os.path.exists(os.path.join('BSDT_control',name)): pass
+    else: os.makedirs(os.path.join('BSDT_control',name))
+    with open(os.path.join('BSDT_control',name,'target.bsdt'), 'w') as f:
+        for target in targetlist:
+            f.write(f'{target}\n')
+
+def bsdtfile(name):
+    target = []
+    with open(os.path.join('BSDT_control',name,'dict.bsdt'), 'r') as f:
+        lines = f.readlines()
+        for line in lines:
+            if line.strip() == '': print('Found empty line. passing...')
+            else: 
+                target.append(line.strip())
+                print(f'Found {line.strip()}')
+    write_targetlist(name,target)
+
+def starting(head,name):
+    target = []
+    for i in os.listdir('.'):
+        j = i.strip()
+        if j.startswith(head): 
+            target.append(j)
+            print(f'Found {j}')
+    write_targetlist(name,target)
+
+def ending(foot,name):
+    target = []
+    for i in os.listdir('.'):
+        j = i.strip()
+        if j.endswith(foot):
+            target.append(j)
+            print(f'Found {j}')
+    write_targetlist(name,target)
