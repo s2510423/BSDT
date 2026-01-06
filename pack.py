db4aaccbec5afc415c1ef08dd91e2cbb0ffd232a
Brainless <s2510423@outlook.kr>
2026-01-06 03:37:30 +0000

    fix: fix dependency mistake + add dependency download logic on pack.py

--- pack.py
+++ pack.py
@@ -5,4 +5,10 @@ from . import header
 
 def boot():
     header.banner()
+    pack_req = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'requirements.txt')
+    subprocess.run([sys.executable, '-m','pip','install','-r',pack_req])
+
+def imp():
+    header.banner()
     subprocess.run([sys.executable, '-m','pip','install','-r','requirements.txt'])
+
