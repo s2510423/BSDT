8576dee5c966215e90b3078f067b6b8e16fdf25a
Brainless <s2621019@siheung.hs.kr>
2026-03-06 20:04:43 +0900

    feat: plotter.py on development

--- bsdt/util/processer/plotter.py
+++ bsdt/util/processer/plotter.py
@@ -1,22 +1,46 @@
 import matplotlib
 import numpy
 import os
-from ..parcer import reader
+import pandas
 
-def plotter(path,dataframe,x_key,y_key):
-    dir = reader
-    if not isinstance(
+def type_check(obj,obj_type):
+    if not isinstance(obj,obj_type): raise TypeError(f'{obj} is required to be {obj_type}')
+
+def plotter(dataframe,x_key,y_keys,y_colors,title='',x_label='',y_label=''):
+    if not (dataframe and x_key and y_keys and y_colors): raise ValueError('from util/processer/plotter.py plotter: not enough arguments')
+    args = {
+        dataframe   :   pandas.DataFrame,
+        x_key       :   str,
+        y_keys      :   dict,
+        title       :   str,
+        x_label     :   str,
+        y_label     :   str
+        }
+    for arg in args:
+        if not type_check(arg, args[arg]): raise TypeError('from util/processer/plotter.py plotter: not proper argument detected')
+    colors = {
+        'black' :   (0.0,0.0,0.0,1.0),
+        'red'   :   (1.0,0.0,0.0,1.0),
+        'green' :   (0.0,1.0,0.0,1.0),
+        'blue'  :   (0.0,0.0,1.0,1.0),
+        'purple':   (1.0,0.0,1.0,1.0),
+        'yellow':   (1.0,1.0,0.0,1.0),
+        'cyan'  :   (0.0,1.0,1.0,1.0),
+        'orange':   (1.0,0.5,1.0,1.0),
+    }
     x = dataframe[x_key]
-    y = dataframe[y_key]
-    matplotlib.plot(timelines_arr, drag_arr, label = "Drag", color = (0.0, 0.0, 0.0, 0.5), linestyle="-", marker="")
-    matplotlib.axhline(y=float(686), label = "Gravity", color = (0.0, 0.0, 1.0, 0.7), linestyle=":")
-    matplotlib.plot(timelines_arr, lift_arr, label = "Lift", color = (1.0, 0.0, 0.0, 1.0), linestyle="-", marker="")
+    y = []
+    for y_key in y_keys:
+        y.append(dataframe[y_key])
+        i = 0
+    while i < len(y):
+        matplotlib.plot(x, y[i], label = y_key, color = y_colors[i], linestyle="-", marker="")
 
-    matplotlib.title(f"Lift and Drag of Human Body [ angle : {self.angle}deg ] [ Wind Velocity : 60m/s ]")
+    matplotlib.title(title)
     matplotlib.xticks([])
     matplotlib.grid(axis='x', visible=False)
-    matplotlib.xlabel("Time         [   s   ]")
-    matplotlib.ylabel("Force        [   N   ]")
+    matplotlib.xlabel(x_label)
+    matplotlib.ylabel(y_label)
     matplotlib.grid(True)
     matplotlib.legend(
         loc       = "lower right",
@@ -25,8 +49,5 @@ def plotter(path,dataframe,x_key,y_key):
         facecolor = "white",
         )
 
-    matplotlib.savefig(f"single_forces_{self.angle}.png", dpi=300, bbox_inches='tight')
-    timelines.clear()
-    lift_list.clear()
-    drag_list.clear()
+    matplotlib.savefig(f"{title}.png", dpi=300, bbox_inches='tight')
     matplotlib.close()
